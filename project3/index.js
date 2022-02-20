0db2152b1ea65f73ac2b84a26ebb8cdb7566f8ce
Anurag Timilsina <anurag.timilsina@gmail.com>
2022-02-20 10:56:00 +0545

    Finished making the layout of the website

--- project3/index.js
+++ project3/index.js
@@ -1,2 +1,22 @@
 console.log("Index.js File!!!");
 //c3deea4efcb344ba8621a2eb32304ea7
+
+let newsAccordion = document.getElementById("newsAccordion");
+
+let news = `
+<p>
+<button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample"
+    aria-expanded="false" aria-controls="collapseWidthExample">
+    News Heading!
+</button>
+</p>
+<div style="min-height: 120px;">
+<div class="collapse collapse-horizontal" id="collapseWidthExample">
+    <div class="card card-body" style="width: 300px;">
+        This is some placeholder content for a horizontal collapse. It's hidden by default and shown when
+        triggered.
+    </div>
+</div>
+</div>
+`;
+
